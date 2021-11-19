82152151dc62e8819f52aeb1925dedb8be9c063a
everybees <everybees@gmail.com>
2021-11-19 16:00:34 +0100

    disturbing day

--- users/models.py
+++ users/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
 
+USER_TYPE = (
+    ('sender', "Sender"),
+    ('transporter', "Transporter"),
+)
+
+
 class Account(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=225)
+    user_type = models.CharField(max_length=50, choices=USER_TYPE, default='sender')
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    image = models.TextField()
+    address = models.TextField()
+    username = models.CharField(max_length=20)
 
     def __str__(self):
         return self.email
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework import viewsets, status
@@ -11,21 +12,33 @@ class UserViewSet(viewsets.ViewSet):
 
     @action(detail=False, methods=['post'])
     def create_account(self, request):
-        soo = request.data
-        if "email" not in soo or "password" not in soo or "confirm_password" not in soo:
-            return Response({"message": "Your data is incorrect"}, status=status.HTTP_400_BAD_REQUEST)
-        if soo.get('password') != soo.get('confirm_password'):
-            return Response({"message": "Password do not match."}, status=status.HTTP_400_BAD_REQUEST)
-        Account.objects.create(email=soo.get('email'), password=soo.get('password'))
-        return Response({"message": "success!"}, status=status.HTTP_200_OK)
+        try:
+            soo = request.data
+            if "email" not in soo or "password" not in soo or "confirm_password" not in soo or "user_type" not in soo:
+                return Response({"message": "Your data is incorrect"}, status=status.HTTP_400_BAD_REQUEST)
+            if soo.get('password') != soo.get('confirm_password'):
+                return Response({"message": "Password do not match."}, status=status.HTTP_400_BAD_REQUEST)
+            Account.objects.create(
+                email=soo.get('email'), password=soo.get('password'), user_type=soo.get('user_type')
+            )
+            return Response({"message": "success!"}, status=status.HTTP_200_OK)
+        except IntegrityError:
+            return Response({"message": "A user with this email exists."}, status=status.HTTP_400_BAD_REQUEST)
 
     @action(detail=False, methods=['post'])
     def login(self, request):
-        return render(request, "login.html")
+        try:
+            data = request.data
+            if "email" not in data or "password" not in data:
+                return Response({"message": "Your data is incorrect"}, status=status.HTTP_400_BAD_REQUEST)
+            user = Account.objects.get(email=data.get('email'))
+            if user.password == data.get("password"):
+                return Response({"message": "Login Successful"}, status=status.HTTP_200_OK)
+            return Response({"message": "This password is incorrect"}, status=status.HTTP_400_BAD_REQUEST)
+        except Account.DoesNotExist:
+            return Response({"message": "User with this email does not exist."}, status=status.HTTP_404_NOT_FOUND)
 
-    def create_profile(self, request):
-        return render(request, "create_profile.html")
-
+    @action(detail=False, methods=['put'])
     def update_profile(self, request):
         return render(request, "update_profile.html")
 
